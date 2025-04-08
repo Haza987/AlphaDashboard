62aec2b44a53f3d96a0220f2f00ea28cf5f2abfa
Harry Holmes <harryholmesse@gmail.com>
2025-04-08 16:20:58 +0200

    Added basic controllers and views

---
+++ WebApp/Controllers/AuthController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp.Controllers
+{
+    public class AuthController : Controller
+    {
+        #region Sign Up
+
+        public IActionResult SignUp()
+        {
+            ViewBag.Title = "Sign Up";
+            return View();
+        }
+        #endregion
+
+        #region Sign In
+        public IActionResult SignIn()
+        {
+            ViewBag.Title = "Sign In";
+            return View();
+        }
+
+        #endregion
+
+        #region Sign Out
+        public new IActionResult SignOut()
+        {
+            return View();
+        }
+        #endregion
+    }
+}
