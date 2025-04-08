ed9d139165b3eeabc5dbefd8956ad8dba6a9cfdc
Harry Holmes <harryholmesse@gmail.com>
2025-04-08 16:58:03 +0200

    Created Data context with databse

--- WebApp/Program.cs
+++ WebApp/Program.cs
@@ -1,6 +1,10 @@
+using Data.Contexts;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 
+builder.Services.AddDbContext<DataContext>(x => x.UseSqlServer(builder.Configuration.GetConnectionString("SqlConnection")));
 
 
 var app = builder.Build();
