89b46ed95e4ee1b9c89dbcd6c14ba35af1d0c308
Harry Holmes <harryholmesse@gmail.com>
2025-04-17 16:23:08 +0200

    Added the base for Projects

--- WebApp/Controllers/ProjectsController.cs
+++ WebApp/Controllers/ProjectsController.cs
@@ -1,25 +1,36 @@
-﻿using Data.Contexts;
+﻿using Business.Interfaces;
+using Business.Models;
+using Data.Contexts;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
+using WebApp.ViewModels;
 
 namespace WebApp.Controllers
 {
-    public class ProjectsController(DataContext context) : Controller
+    public class ProjectsController(DataContext context, IProjectService projectService) : Controller
     {
         private readonly DataContext _context = context;
+        private readonly IProjectService _projectService = projectService;
 
         [Route("projects")]
-        public IActionResult Projects()
+        public async Task<IActionResult> Projects()
         {
             int projectCount = _context.Projects.Count();
             int projectInProgress = _context.Projects.Count(p => !p.IsCompleted);
             int projectComplete = _context.Projects.Count(p => p.IsCompleted);
 
+            var projects = await _projectService.GetAllProjectsAsync();
+
+            var viewModel = new ProjectViewModel
+            {
+                Projects = projects?.ToList() ?? new List<Project>()
+            };
+
             ViewBag.Title = "Projects";
             ViewBag.ProjectCount = projectCount;
             ViewBag.ProjectInProgress = projectInProgress;
             ViewBag.ProjectComplete = projectComplete;
-            return View();
+
+            return View(viewModel);
         }
     }
 }
---
+++ WebApp/ViewModels/ProjectViewModel.cs
@@ -0,0 +1,8 @@
+﻿using Business.Models;
+
+namespace WebApp.ViewModels;
+
+public class ProjectViewModel
+{
+    public List<Project> Projects { get; set; } = [];
+}
